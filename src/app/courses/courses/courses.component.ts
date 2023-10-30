6e13fce92b6575a97ca5aa120d8ea485d4a47dbf
Leonardo Azzola <uleowz@gmail.com>
2023-10-30 09:37:03 -0300

    Criando um Service no Angular

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,6 @@
+import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
+import { Course } from '../model/courses';
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
 
+
+
+  constructor(private coursesService: CoursesService){
+    // this.courses = [];
+    // this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
+  }
 }
