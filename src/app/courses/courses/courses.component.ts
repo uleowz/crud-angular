db2fbb47f943de86461c7221b2d3b4ab555f5e82
Leonardo Azzola <uleowz@gmail.com>
2023-11-08 19:01:06 -0300

    Componente de Formulário e Roteamento para criar cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, Observable, of } from 'rxjs';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
@@ -13,11 +14,13 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CoursesComponent {
   courses$: Observable<Course[]>;
-  displayedColumns = ['name', 'category'];
+  displayedColumns = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
@@ -34,4 +37,9 @@ export class CoursesComponent {
       data: errorMsg,
     });
   }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
 }
