6f6bb71180851f56078f456a0f2b1818b2d1e9df
Leonardo Azzola <uleowz@gmail.com>
2023-12-19 13:11:18 -0300

    Atualizar Curso: Salvando o Formulário

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -22,7 +22,22 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>){
+    // console.log(record);
+    if (record._id) {
+      // console.log('update');
+      return this.update(record);
+    }
+    // console.log('create');
+    return this.create(record);
+
+  }
+
+  private create(record: Partial<Course>){
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record);
   }
 
 
